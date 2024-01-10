ab0978ef0b276238a765bcfce048e914190fd3a4
Cadence Reynolds <reynocal001@warren.k12.in.us>
2024-01-10 08:53:48 -0500

    Lesson 3

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/03_Statements, Syntax, Comments, Variables, and Prompt/script.js
@@ -0,0 +1,4 @@
+let FirstName = prompt("Please enter your first name");
+let LastName = prompt("Please enter your last name");
+
+document.getElementById("name").innerHTML = FirstName + " " + LastName;
